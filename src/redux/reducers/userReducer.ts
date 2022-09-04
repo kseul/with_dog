182592a2aaebfb37115b6ce906c8e738d73b4e25
SeungJu <asjj8051@gmail.com>
2022-09-04 21:00:25 +0900

    redux 초기 세팅

---
+++ src/redux/reducers/userReducer.ts
@@ -0,0 +1,27 @@
+const initialState = {
+  loggedIn: true,
+  id: 1,
+  name: '안성주',
+  nickname: '안론머스크',
+  email: 'wkddb1359@naver.com',
+  user_type: 'email',
+  status: 'active',
+  account_type: 'normal',
+  thumbnail_url: 'nope',
+  mbti: null,
+};
+
+const userReducer = (prevState = initialState, action) => {
+  switch (action.type) {
+    case 'LOG_IN':
+      return { ...prevState, loggedIn: action.data };
+    case 'LOG_OUT':
+      return { ...prevState, loggedIn: action.data };
+    case 'SET_MBTI':
+      return { ...prevState, mbti: action.data };
+    default:
+      return prevState;
+  }
+};
+
+export default userReducer;
