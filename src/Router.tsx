5873832809e29a3837a121e45478ceb44fdba500
cham-min <hymn9613@gmail.com>
2022-09-07 17:39:31 +0900

    feat: Add Quill Library
    - npm install react-quill
    - 게시글 페이지의 글 작성 버튼을 Link Component로 교체하여 글 작성 페이지로 이동
    - QuillEditor.tsx 컴포넌트 생성

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,7 @@ import MainRouter from './pages/MainRouter/MainRouter';
 import Main from 'pages/Main/Main';
 import ChatRoom from 'pages/Chatting/ChatRoom';
 import KakaoLogin from 'pages/Login/SignIn/kakaoLogin/KakaoLogin';
+import QuillEditor from 'pages/NoticeBoard/QuillEditor/QuillEditor';
 
 const Router = () => {
   return (
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/noticeboard" element={<NoticeBoard />} />
+        <Route path="/noticeboard/write" element={<QuillEditor />} />
         <Route path="/*" element={<MainRouter />} />
         <Route path="/admin/:value/*" element={<Admin />} />
         <Route path="/chat" element={<ChatRoom />} />
