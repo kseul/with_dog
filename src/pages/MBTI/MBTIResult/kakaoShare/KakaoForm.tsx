35295bf5821d0d68ae368a64f9ac0af24c1c80a5
Kyeongyeon Yun <kyeongyeonyun@Yuntrollpark-2.local>
2022-09-18 17:50:08 +0900

    feat: SNS 공유 기능 추가

---
+++ src/pages/MBTI/MBTIResult/kakaoShare/KakaoForm.tsx
@@ -0,0 +1,28 @@
+import kakaoShare from 'assets/images/kakaoShare.png';
+const { Kakao, location }: any = window;
+
+export const shareKakao = () => {
+  const sharedUrl = location.href;
+
+  Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '함께하개',
+      description: '댕댕이 MBTI 검사결과 보러가기',
+      imageUrl: kakaoShare,
+      link: {
+        webUrl: sharedUrl,
+        mobileWebUrl: sharedUrl,
+      },
+    },
+    buttons: [
+      {
+        title: '자세히 보기',
+        link: {
+          webUrl: sharedUrl,
+          mobileWebUrl: sharedUrl,
+        },
+      },
+    ],
+  });
+};
