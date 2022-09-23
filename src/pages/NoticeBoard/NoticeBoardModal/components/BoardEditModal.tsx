0afd4df7583a79cd677dafd99a82c1a6e65850d3
cham-min <hymn9613@gmail.com>
2022-09-23 16:32:34 +0900

    feat: 게시글 삭제 기능 추가
    - fetch 함수로 delete 통신 기능 추가

--- src/pages/NoticeBoard/NoticeBoardModal/components/BoardEditModal.tsx
+++ src/pages/NoticeBoard/NoticeBoardModal/components/BoardEditModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -8,10 +9,33 @@ interface editModalProps {
 
 const BoardEditModal = ({ handleEditModal }: editModalProps) => {
   const navigate = useNavigate();
+  const boardData = useSelector((state: any) => state.board.boardData);
 
   const handleEdit = () => {
     navigate('/noticeBoard/modify');
   };
+
+  const handleDelete = async () => {
+    const formData = new FormData();
+    formData.append('delete_reason', 'test');
+
+    await fetch(
+      `https://togedog-dj.herokuapp.com/posts/${boardData.id}/delete/`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjo5LCJ1c2VyX3R5cGUiOiJhZG1pbiIsImV4cCI6MTY2NDY4NTQ5MiwiaWF0IjoxNjYyMDkzNDkyfQ.AQAciBT2VhdUDY-rQuoRiJCXE3BfIQJd95KgCXk0eKU`,
+        },
+        body: formData,
+      }
+    );
+    return window.location.reload();
+  };
+
+  const EDITMODAL_LIST = [
+    { id: 1, page: '수정하기', handle: handleEdit },
+    { id: 2, page: '삭제하기', handle: handleDelete },
+  ];
 
   return (
     <EditModalContainer onClick={handleEditModal}>
@@ -20,9 +44,9 @@ const BoardEditModal = ({ handleEditModal }: editModalProps) => {
           e.stopPropagation();
         }}
       >
-        {EDITMODAL_LIST.map(({ id, page }) => {
+        {EDITMODAL_LIST.map(({ id, page, handle }) => {
           return (
-            <Edit key={id} onClick={handleEdit}>
+            <Edit key={id} onClick={handle}>
               {page}
             </Edit>
           );
@@ -31,8 +55,6 @@ const BoardEditModal = ({ handleEditModal }: editModalProps) => {
     </EditModalContainer>
   );
 };
-
-const EDITMODAL_LIST = [{ id: 1, page: '수정하기' }];
 
 const EditModalContainer = styled.div`
   position: fixed;
